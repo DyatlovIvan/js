50a5d0ef7754d928cdae7cfbb5460f7985cf0532
DyatlovIvan <diatlovivan92@mail.ru>
2021-12-06 16:29:40 +0400

    lesson4

--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,3 +1,6 @@
+import {Simulate} from "react-dom/test-utils";
+
+
 console.log('lesson 4');
 
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
@@ -8,11 +11,19 @@ console.log('lesson 4');
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
 
+// const promise  = new Promise((resolve,rejects)=>{
+//     console.log('Promise is created')
+// });
+
 
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
+
+const promise = Promise.resolve().then(() => {
+    console.log('Promise Data')
+})
 
 
 // Task 03
@@ -20,12 +31,25 @@ console.log('lesson 4');
 // и возвращает строку 'Promise Error'
 // Получите данные промиса и выведите их в консоль
 
+const promiseRej = Promise.reject().catch(() => {
+    console.log('Promise Error')
+})
 
 // Task 04
 // Создайте промис, который переходит в состояние resolved через 3с.
 // (Используйте setTimeout)
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
+
+new Promise((resolve, rejects) => {
+
+    setTimeout(() => {
+    return resolve(0)
+    }, 3000)
+}).then((resolve)=>{
+    console.log(1111)
+})
+
 
 
 // Task 05
@@ -177,7 +201,7 @@ window.testProm = handlePromise;
 //     }, ms*100);
 // }
 
-async function sleep(ms:number) {
+async function sleep(ms: number) {
     return new Promise((res, rej) => {
         setTimeout(() => {
             //res()
@@ -196,8 +220,6 @@ async function show() {
 show();
 
 
-
-
-
 // just a plug
-export default ()=>{};
+export default () => {
+};
